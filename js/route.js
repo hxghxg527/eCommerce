f5cbfad7fe2be17f049735a168586ba70c6a983f
hxghxg527 <hxghxg527@163.com>
2016-01-02 00:38:07 +0800

    test router

--- js/route.js
+++ js/route.js
@@ -6,17 +6,25 @@
 
 angular.module('ecRoute', ['ui.router'])
     .config(function ($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.when('', '/mainPage');
+        $urlRouterProvider
+            .when('', '/mainPage')
+            .otherwise('/mainPage');
 
         $stateProvider.state('mainPage', {
             url: '/mainPage',
             templateUrl: 'templates/mainPage.html',
             controller: 'ecMainPageController'
-        }).state('mainPage.test', {
-            url: '/test',
-            templateUrl: 'templates/test.html'
+        }).state('mainPage.test1', {
+            url: '/test1',
+            templateUrl: 'templates/test1.html'
+        }).state('mainPage.test2', {
+            url: '/test2',
+            templateUrl: 'templates/test2.html'
+        }).state('test3', {
+            url: '/test3',
+            templateUrl: 'templates/test3.html'
         });
     })
     .run(function ($rootScope) {
-        $rootScope.name = 'hello world';
+        $rootScope.name = 'run...';
     });
