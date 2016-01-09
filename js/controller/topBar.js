c9159d843ce344c5b9e9016d04c68b3f4ad86d72
hxghxg527 <hxghxg527@163.com>
2016-01-09 11:58:09 +0800

    get shopping cart by ajax and set shopping cart num into html.

--- js/controller/topBar.js
+++ js/controller/topBar.js
@@ -5,6 +5,17 @@
 'use strict';
 
 angular.module('ec.controller.topBar', [])
-    .controller('ecTopBarController', function ($scope, $rootScope) {
+    .controller('ecTopBarController', function ($scope, $rootScope, ecInteractionService, $timeout) {
         $scope.currentUserInfo = $rootScope.currentUserInfo;
+
+
+        if ($rootScope.isLogin) {
+            ecInteractionService.getShoppingCart(function (shoppingCart) {
+                $timeout(function () {
+                    if (shoppingCart && shoppingCart != 'undefined') {
+                        $scope.shoppingCartGoods = shoppingCart.shoppingCartGoods;
+                    } else $scope.shoppingCartGoods = [];
+                }, 1000);
+            });
+        } else $scope.shoppingCartGoods = [];
     });
