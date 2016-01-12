96d10549ffb9e38cc9e111a09cb0ccb964dc39b0
hxghxg527 <hxghxg527@163.com>
2016-01-12 21:34:38 +0800

    refine main menu data.

--- js/controller/menu/subMenu.js
+++ js/controller/menu/subMenu.js
@@ -5,9 +5,9 @@
 'use strict';
 
 angular.module('ec.controller.subMenu', [])
-    .controller('ecSubMenuController', function ($scope, ecInteractionService, ecConstant) {
+    .controller('ecSubMenuController', function (ecInteractionService, ecConstant) {
         var self = this;
-        
+
         ecInteractionService.getSubMenu(function (status, subMenu) {
             if (status == ecConstant.SUCCESS && subMenu) {
                 self.subMenuList = subMenu.subMenuList || [];
