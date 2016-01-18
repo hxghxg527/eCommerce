1521699f7e92e6dc18b586870cbdf9942b738d89
hxghxg527 <hxghxg527@163.com>
2016-01-19 00:07:26 +0800

    refine code.

--- js/controller/menu/mainMenu.js
+++ js/controller/menu/mainMenu.js
@@ -13,4 +13,21 @@ angular.module('ec.controller.mainMenu', [])
                 self.mainMenuList = mainMenu.mainMenuList || [];
             }
         });
+
+        $scope.changeMenuModel = function (idx) {
+            $scope.menu = self.mainMenuList[idx];
+            $scope.activeIndex = idx;
+        };
+
+        $scope.leave = function () {
+            $scope.menu = null;
+            $scope.activeIndex = -1;
+        };
+
+        $scope.$watch(function () {
+            return $scope.activeIndex;
+        }, function (activeIndex) {
+            $('.first-level-menu-li').removeClass('active');
+            if (activeIndex > -1) $('.first-level-menu-li').eq(activeIndex).addClass('active');
+        });
     });
