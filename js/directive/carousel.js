655e9bfdd7842cf9b5aeb69dd7d55bcafb1098af
hxghxg527 <hxghxg527@163.com>
2016-01-22 00:09:43 +0800

    add carousel effect.

--- js/directive/carousel.js
+++ js/directive/carousel.js
@@ -5,7 +5,7 @@
 'use strict';
 
 angular.module('ec.directive.carousel', [])
-    .directive('ecCarousel', function () {
+    .directive('ecCarousel', function ($timeout) {
         return {
             restrict: 'EA',
             templateUrl: 'templates/carousel.html',
@@ -15,7 +15,37 @@ angular.module('ec.directive.carousel', [])
             controller: '',
             controllerAs: 'ecCarousel',
             link: function (scope, element, attrs) {
+                var carouselEl = $(element),
+                    contentContainerEl = carouselEl.find('.ec-carousel-outer-container'),
+                    indicatorPoints = carouselEl.find('.ec-carousel-indicators-point'),
+                    timer = null;
 
+                indicatorPoints.bind('mouseenter', function () {
+                    indicatorPoints.removeClass('selected');
+                    $(this).addClass('selected');
+                    var idx = $(this).index();
+
+                    if (timer) {
+                        $timeout.cancel(timer);
+                        timer = null;
+                    }
+                    timer = $timeout(function () {
+                        $timeout.cancel(timer);
+                        timer = null;
+
+                        contentContainerEl.stop(true).animate({
+                            left: '-' + idx + '00%'
+                        }, 'slow');
+                    }, 1000);
+                });
+
+                scope.$destroy(function () {
+                    contentContainerEl.stop();
+                    if (timer) {
+                        $timeout.cancel(timer);
+                        timer = null;
+                    }
+                });
             }
         };
     });
